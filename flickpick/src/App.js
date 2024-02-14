7d9c413494310442586e59617397963984a066f0
Logan <logan.nightingale@ucalgary.ca>
2024-02-14 08:31:27 -0700

    React project setup and like/dislike

---
+++ flickpick/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Movies from './MovieCards.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Movies></Movies>
+    </div>
+  );
+}
+
+export default App;
