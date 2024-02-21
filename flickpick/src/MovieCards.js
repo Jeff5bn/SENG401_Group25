9b956ab9502f0ce2df101850a12d14f6291d3baa
Logan <logan.nightingale@ucalgary.ca>
2024-02-20 17:33:44 -0700

    Style changes and continue swiping

--- flickpick/src/MovieCards.js
+++ flickpick/src/MovieCards.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './MovieCards.css';
 
-const Duration = 200;
+const Duration = 300;
 
 const movies = [
   { id: 1, name: "Movie 1", genre: "Action", imgUrl: "https://image.tmdb.org/t/p/original///7lTnXOy0iNtBAdRP3TZvaKJ77F6.jpg" },
@@ -15,14 +15,24 @@ const movies = [
   { id: 9, name: "Movie 9", genre: "Fantasy", imgUrl: "https://image.tmdb.org/t/p/original///35Uef7fz9ctYbJLXbJBCqvtttEQ.jpg" },
   { id: 10, name: "Movie 10", genre: "Adventure", imgUrl: "https://image.tmdb.org/t/p/original///5jGKbYuZtdxSNOocI6ZziQeiY4n.jpg" },
   { id: 11, name: "Movie 11", genre: "Documentary", imgUrl: "https://image.tmdb.org/t/p/original///FgFoX4wlZUT31ErGGzVnNlh8U3.jpg" },
+  { id: 12, name: "Movie 12", genre: "Action", imgUrl: "https://image.tmdb.org/t/p/original///7lTnXOy0iNtBAdRP3TZvaKJ77F6.jpg" },
 ];
 
 function Movies() {
+  const [swipeState, setSwipeState] = useState(0); 
   const [currentIndex, setCurrentIndex] = useState(0);
   const [swipeDirection, setSwipeDirection] = useState(null);
   
   const handleSwipe = (direction) => {
     setSwipeDirection(direction);
+    // Check if the initial 10 swipes have been completed
+    if(currentIndex === 9){
+      setSwipeState(1);
+    }
+    // Set the state if the index is out of bounds
+    if(currentIndex + 1 >= movies.length){
+      setSwipeState(3);
+    }
     // Code to handle like/skip/dislike
     setTimeout(() => {
       setCurrentIndex(currentIndex + 1);
@@ -32,10 +42,10 @@ function Movies() {
 
   return (
     <div className="Movie">
-      {currentIndex < 10 ? (
+      {swipeState === 0 ? (
         <div className={`movie-card ${swipeDirection === 'dislike' ? 'slide-out-left' : swipeDirection === 'like' ? 'slide-out-right' : swipeDirection === 'skip' ? 'slide-out-up' : ''}`}>
-          <img src={movies[currentIndex].imgUrl} alt={movies[currentIndex].name} />
-          <div>
+          <div id="background">
+            <img src={movies[currentIndex].imgUrl} alt={movies[currentIndex].name} />
             <h2>{movies[currentIndex].name}, {movies[currentIndex].genre}</h2>
             <div className="buttons">
               <button onClick={() => handleSwipe('dislike')}>❌</button>
@@ -44,18 +54,38 @@ function Movies() {
             </div>
           </div>
         </div>
-      ) : (
-        <div className="Recommend">
+      ) : swipeState === 1 ? (
+        <div className="center-column-div">
           <div className="movie-card" style={{opacity: "0.2"}}>
-            <img src={movies[currentIndex].imgUrl} alt={movies[currentIndex].name} />
-            <div>
+          <div id="background">
+              <img src={movies[currentIndex].imgUrl} alt={movies[currentIndex].name} />
               <h2>{movies[currentIndex].name}, {movies[currentIndex].genre}</h2>
             </div>
           </div>
           <div className="rec-buttons">
             <button className="basic-button">See Recommendations</button>
-            <button className="basic-button">Continue Swiping</button>
+            <button className="basic-button" onClick={() => setSwipeState(2)}>Continue Swiping</button>
           </div>
+        </div>
+      ) : swipeState === 2 ? (
+        <div className="center-column-div">
+          <button className="basic-button">See Recommendations</button>
+          <div className={`movie-card ${swipeDirection === 'dislike' ? 'slide-out-left' : swipeDirection === 'like' ? 'slide-out-right' : swipeDirection === 'skip' ? 'slide-out-up' : ''}`}>
+            <div id="background">
+              <img src={movies[currentIndex].imgUrl} alt={movies[currentIndex].name} />
+              <h2>{movies[currentIndex].name}, {movies[currentIndex].genre}</h2>
+              <div className="buttons">
+                <button onClick={() => handleSwipe('dislike')}>❌</button>
+                <button onClick={() => handleSwipe('skip')}>⛔</button>
+                <button onClick={() => handleSwipe('like')}>❤️</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="center-column-div">
+          <h1>End of movie list!</h1>
+          <button className="basic-button">See Recommendations</button>
         </div>
       )}
     </div>
